9b16873d575ced16feb0551380142498d98a7767
unknown <daniilpetrov776@gmail.com>
2025-06-06 21:49:34 +0300

    daily updates WIP

--- people-wise/app/app.tsx
+++ people-wise/app/app.tsx
@@ -10,6 +10,40 @@ import { getIsDataLoading } from '@/store/people-data/selectors';
 import { initDB } from '@/data-base/db';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat'
+import * as BackgroundTask from 'expo-background-task';
+import * as TaskManager from 'expo-task-manager';
+import { store } from "@/store";
+
+const BACKGROUND_TASK_BIRTHDAY_UPDATE = 'BACKGROUND_BIRTHDAY_UPDATE';
+
+const dispatch = store.dispatch;
+
+TaskManager.defineTask(BACKGROUND_TASK_BIRTHDAY_UPDATE, async () => {
+  try {
+    await initDB();
+    dispatch(syncPersonsFromDB())
+    console.log('[TaskManager] фоновая задачу запущена');
+  } catch (error) {
+    console.log('[TaskManager] Ошибка при выполнении фоновой задачи:', error);
+    return BackgroundTask.BackgroundTaskResult.Failed;
+  }
+  return BackgroundTask.BackgroundTaskResult.Success;
+})
+
+const registerBackgroundTaskAsync = async () => {
+  const status = await BackgroundTask.getStatusAsync();
+  if (status === BackgroundTask.BackgroundTaskStatus.Restricted) {
+    return;
+    }
+
+    const tasks = await TaskManager.getRegisteredTasksAsync();
+    if (!tasks.some((task) => task.taskName === BACKGROUND_TASK_BIRTHDAY_UPDATE)) {
+      await BackgroundTask.registerTaskAsync(BACKGROUND_TASK_BIRTHDAY_UPDATE, {
+        minimumInterval: 15 * 60, // 15 минут
+      })
+      console.log('Background task для пересчёта дней до дня рождения зарегистрирован');
+    }
+}
 
 type Props = {
   colorScheme: 'dark' | 'light' | null;
@@ -22,6 +56,7 @@ export default function AppLayout({ colorScheme }: Props) {
   const isDataLoading = useAppSelector(getIsDataLoading);
   const [dbReady, setDbReady] = React.useState(false);
 
+
 useEffect(() => {
     const prepareApp = async () => {
       try {
@@ -29,6 +64,17 @@ useEffect(() => {
         console.log('[DB] Инициализация завершена');
         setDbReady(true);
         dispatch(syncPersonsFromDB()); // Только после initDB
+        registerBackgroundTaskAsync(); // Регистрация фоновой задачи
+
+        const interval = setInterval(async () => {
+          const now = dayjs();
+          if (now.hour() === 0 && now.minute() === 0) {
+            await initDB();
+            dispatch(syncPersonsFromDB());
+          }
+        }, 60000); // Проверка каждую минуту
+        return () => clearInterval(interval);
+
       } catch (e) {
         console.error('[RootLayout] Ошибка при инициализации БД:', e);
       }
